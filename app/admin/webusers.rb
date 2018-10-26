705b864f7d70a407fe4c744deacf711ad09fe050
oxygen545 <damon@hopkins-family.org>
2018-10-25 20:13:18 -0500

    style edits

--- app/admin/webusers.rb
+++ app/admin/webusers.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Webuser do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-permit_params :name, :email, :email_confirmation
+permit_params :name, :email, :password, :password_confirmation
 
   index do
     selectable_column
